2dedb6d7030d14785d9798ec11f7e573681f08d2
caiozenke <74631392+caiozenke@users.noreply.github.com>
2022-09-28 17:31:07 -0300

    try e except aulas

--- Modulo2/a.py
+++ Modulo2/a.py
@@ -8,14 +8,14 @@ para um n informado pelo usuário. Use uma função que receba um valor n inteir
 
 
 
-u = int(input('a'))
+#u = int(input('a'))
 
 def ex1(n):
     num = u
     for i in range(n):
         print(n, end =' ')
 
-ex1(u)
+#ex1(u)
 
 """Embaralha palavra.
 Construa uma função que receba uma string como parâmetro e devolva outra string com os carateres embaralhados. 
@@ -30,36 +30,12 @@ def embaralha(s):
     ns = ''.join(nl)
     return print(ns)    
 
-embaralha('python')
+#embaralha('python')
 
 
-"""Desenha moldura. 
-Construa uma função que desenhe um retângulo usando os caracteres ‘+’ , ‘−’ e ‘| ‘. 
-Esta função deve receber dois parâmetros, linhas e colunas, sendo que o valor por omissão é o valor mínimo igual a 1 e o valor máximo é 20. 
-Se valores fora da faixa forem informados, eles devem ser modificados para valores dentro da faixa de forma elegante.
-"""
 
 
-ll = input("Digite Onumero de linhas: ")
-cl = input('Digite po numero de Colunas:' )
 
-
-    
-if ll or cl == '' or 0 or 0 or '':
-       linhas , colunas = 1 , 20
-
-       
-l1=int(ll)
-col = int(cl) -1 
-
-adi = '+'
-for i in range(l1):
-    adi += '-'
-  
-adi += '+'
-    
-    
-print(adi)  
 
         
 
