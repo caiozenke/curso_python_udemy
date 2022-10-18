554149ae3ace4d9c38467aaca8407be48a037567
caiozenke <74631392+caiozenke@users.noreply.github.com>
2022-10-18 18:39:40 -0300

    aula sobre metodo estatico

--- Modulo3/metodo_estaticos.py
+++ Modulo3/metodo_estaticos.py
@@ -1,21 +1,28 @@
 from datetime import datetime
-
+from random import randint
 class Pessoa():
     ano_atual = int(datetime.strftime(datetime.now(), '%Y'))#Instancia da classe
     
     
-    def __init__(self,nome ,idade) -> None:
+    def __init__(self,nome ,idade,) -> None:
         self.nome = nome 
         self.idade = idade
-        
-        
-    #metodo de classes = CLS     
+ 
+    
+    
     @classmethod
                     #cls no lugar do self, e os args
     def criar_nascimento(cls,nome, ano_nascimento):
             idade = cls.ano_atual - ano_nascimento
             return cls(nome, idade) #retorna o nome e iade com base do ano passado
- 
-            
+    
+    
+        #decorador do static method
+    @staticmethod #nao precisa ed classe e  instancia
+    def gerar_id():
+            rand = randint(0,100)
+            return rand
+        
 p1 = Pessoa.criar_nascimento('caio', 2005)
-print(p1.nome , p1.idade)# printar a classe
+
+print(p1.gerar_id())
