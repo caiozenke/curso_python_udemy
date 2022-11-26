6a17bb04eee77f46eb776bddab45f8089b6d7e73
Caio Luan Zenke <74631392+caiozenke@users.noreply.github.com>
2022-11-26 19:23:49 -0300

    args exercio facil

---
+++ Modulo2/args.exer.py
@@ -0,0 +1,31 @@
+# Exercícios com funções
+
+# Crie uma função que multiplica todos os argumentos
+# não nomeados recebidos
+# Retorne o total para uma variável e mostre o valor
+# da variável.
+
+def multiplica(*args):
+    total = 1
+
+    for num in args:
+        total *= num
+    
+    return total
+
+
+# Crie uma função fala se um número é par ou ímpar.
+# Retorne se o número é par ou ímpar.
+
+def par_impar(num):
+    
+    par = num % 2 == 0
+
+    if par:
+        return f'{num} é par'
+    return f'{num} é impar'
+
+
+numero = multiplica(3,3)
+print(par_impar(numero))
+
